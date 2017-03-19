44f509056c2cfeeaf1b9ab1550a3892ecd333fa2
Anton L <diss1dent@mail.ru>
2017-03-19 23:47:15 +0200

    feature / forms update

--- src/app/app-component.ts
+++ src/app/app-component.ts
@@ -1,10 +1,47 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
     selector: 'finder-app',
     templateUrl: 'app-component-template.html'
 })
-export class AppComponent {}
+
+export class AppComponent implements OnInit {
+
+    loginForm: FormGroup;
+    registerForm: FormGroup;
+
+    constructor (
+        private fb: FormBuilder
+    ){}
+
+    createLoginForm() {
+        this.loginForm = this.fb.group({
+            login_name: ['', Validators.required],
+            login_password: ['', Validators.required]
+        })
+    }
+
+    createRegisterForm() {
+        this.registerForm = this.fb.group({
+            register_name: ['', Validators.required],
+            register_password: ['', Validators.required]
+        })
+    }
+
+    login() {
+        console.log('login');
+    }
+
+    register() {
+        console.log('login');
+    }
+
+    ngOnInit(): void {
+        this.createRegisterForm();
+        this.createLoginForm();
+    }
+}
 
 // var loginApp = {
 //     name: 'test',
--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -2,13 +2,14 @@ import { NgModule }            from "@angular/core";
 import { BrowserModule }       from "@angular/platform-browser";
 import { HttpModule }          from '@angular/http';
 import { ReactiveFormsModule } from '@angular/forms';
+
 import { AppComponent }        from "./app-component";
 
 @NgModule({
     imports:      [
         BrowserModule,
         HttpModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
     ],
     declarations: [
         AppComponent
